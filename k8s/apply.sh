54974a8e79e11e5eb3fcc342302014c03af91d92
Artur Yumaev <art.yumaev@gmail.com>
2022-07-22 20:16:59 +0300

    Updated scripts

--- k8s/apply.sh
+++ k8s/apply.sh
@@ -1,5 +1,13 @@
+env_file="./env.local"
+namespace=shopper-db
+config_name=postgres-config-map
+
 kubectl apply -f ./namespace.yaml
-kubectl apply -f ./configMap.yaml
+
+# Creating configmap from file
+kubectl create configmap $config_name --from-env-file=$env_file -n $namespace
+kubectl label configmap $config_name app=postgres -n $namespace
+
 kubectl apply -f ./persistentVolume.yaml
 kubectl apply -f ./persistentVolumeClaim.yaml
 kubectl apply -f ./deployment.yaml
--- k8s/delete.sh
+++ k8s/delete.sh
@@ -1,6 +1,8 @@
-kubectl delete svc postgres-service -n shopper-db
-kubectl delete pvc postgres-pvc -n shopper-db
+namespace=shopper-db
+
+kubectl delete svc postgres-service -n $namespace
+kubectl delete pvc postgres-pvc -n $namespace
 kubectl delete pv postgres-pv
-kubectl delete configmap postgres-config-map -n shopper-db
-kubectl delete deploy postgres -n shopper-db
-kubectl delete namespace shopper-db
+kubectl delete configmap postgres-config-map -n $namespace
+kubectl delete deploy postgres -n $namespace
+kubectl delete namespace $namespace
