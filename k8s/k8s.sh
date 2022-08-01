a840e972211f04c78573b05259f75c7543217011
Artur Yumaev <art.yumaev@gmail.com>
2022-08-02 00:19:56 +0300

    Updated configuration

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -4,15 +4,15 @@ config=postgres-config-map
 
 if [ $1 = 'up' ]
 then
-  kubectl apply -f ./namespace.yaml
+  kubectl apply -f ./$namespace/namespace.yaml
 
   kubectl create configmap $config --from-env-file=$env_file -n $namespace
   kubectl label configmap $config app=postgres -n $namespace
 
   kubectl apply -f ./persistentVolume.yaml
-  kubectl apply -f ./persistentVolumeClaim.yaml
-  kubectl apply -f ./deployment.yaml
-  kubectl apply -f ./service.yaml
+  kubectl apply -f ./$namespace/persistentVolumeClaim.yaml
+  kubectl apply -f ./$namespace/deployment.yaml
+  kubectl apply -f ./$namespace/service.yaml
 elif [ $1 == 'down' ]
 then
   kubectl delete namespace $namespace
